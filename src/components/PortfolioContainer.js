ffae631a4f9937307cc65785fef77c742911a8dc
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-05-28 21:02:20 +0100

    Updated project information

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import HeroImage from "./HeroImage";
-import AboutMe from "./AboutMe";
-import WorkShowcase from "./WorkShowcase";
+// import AboutMe from "./AboutMe";
+// import WorkShowcase from "./WorkShowcase";
 
 // Sub-pages
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import About from "./pages/About";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("About");
 
   // Check value of `currentPage`. Then return component to render.
   const renderPage = () => {
-    if (currentPage === "Home") {
-      console.log("home");
-      return <Home />;
-    }
+    // if (currentPage === "Home") {
+    //   console.log("home");
+    //   return <Home />;
+    // }
     if (currentPage === "About") {
       console.log("about");
       return <About />;
--- src/components/WorkProjects.js
+++ src/components/WorkProjects.js
@@ -3,59 +3,69 @@ import image from "../img/default.jpg";
 const workProjects = {
   work: [
     {
-      title: "BAB 1",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+      title: "Sociify",
+      description:
+        "A simple portfolio site for aspiring developers, built with Node, Express, Handlebars, MySQL, and deployed to Heroku. Use it to create and edit your profile and search/view other peoples on the site.",
+      gitURL: "https://github.com/gmadnani/Sociify",
+      depURL: "https://sociify-fortran.herokuapp.com/",
       thumbnailImg: image,
       mainImg: "./img/default.jpg",
     },
     {
-      title: "BAB 2",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
-      thumbnailImg: image,
-      mainImg: "",
-    },
-    {
-      title: "BAB 3",
+      title: "Birmingham Association Banquets",
       description:
-        "Food finding app lormsdnfsmdnfsd nsdnsds jsndfnwkejf amsn  qmndknsd  q dkqdkqw  asda sd as nkdnkwekjwejkq.",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+        "BAB allows a user to search for a city, town or postcode and uses server-side APIs to find data relating to restaurants in that location.",
+      gitURL: "https://github.com/oli-drew/bab-group-project-by-slackware",
+      depURL: "https://oli-drew.github.io/bab-group-project-by-slackware/",
       thumbnailImg: image,
       mainImg: "",
     },
     {
-      title: "BAB",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+      title: "Tech Blog",
+      description:
+        "CMS-style blog site about all things tech, built with Node, Express, Handlebars, MySQL, and deployed to Heroku. Use it to create and comment on the latests tech trends and news.",
+      gitURL: "https://github.com/oli-drew/technology-blog-bootcamp-hw14",
+      depURL: "https://tech-blog-bootcamp-hw14.herokuapp.com/",
       thumbnailImg: image,
       mainImg: "",
     },
     {
-      title: "BAB",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+      title: "JavaScript Quiz Game",
+      description:
+        "A timed quiz on the fundamentals of JavaScript created as part of a homework assignment. The player has 5 minutes to answer the 10 multiple choice questions.",
+      gitURL:
+        "https://github.com/oli-drew/timed-quiz-on-javascript-fundamentals-for-bootcamp-hw4",
+      depURL:
+        "https://oli-drew.github.io/timed-quiz-on-javascript-fundamentals-for-bootcamp-hw4/",
       thumbnailImg: image,
       mainImg: "",
     },
     {
-      title: "BAB",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+      title: "Weather Dashboard",
+      description:
+        "A dashboard to see the weather outlook for multiple cities, built using the OpenWeather API. This weather dashboard has been designed to help users plan trips by saving previous searches, so the user can easily review the weather for their favourite locations.",
+      gitURL: "https://github.com/oli-drew/weather-dashboard-for-bootcamp-hw6",
+      depURL: "https://oli-drew.github.io/weather-dashboard-for-bootcamp-hw6/",
       thumbnailImg: image,
       mainImg: "",
     },
     {
-      title: "BAB",
-      description: "Food finding app",
-      gitURL: "http://bab.github",
-      depURL: "http://bab.deployed",
+      title: "Work Day Planner",
+      description:
+        "A simple calendar application to manage your time hour by hour during the working day. The working day is split into hourly timeblocks. These timeblocks are colour coded depending on whether they are past, present or future. The user can add events to each timeblock, these events are saved in localstorage.",
+      gitURL:
+        "https://github.com/oli-drew/work-day-scheduler-application-for-bootcamp-hw5",
+      depURL:
+        "https://oli-drew.github.io/work-day-scheduler-application-for-bootcamp-hw5/",
+      thumbnailImg: image,
+      mainImg: "",
+    },
+    {
+      title: "Word Guess",
+      description:
+        "A timed word guessing game. The user has 30 seconds to guess the word. When a user correctly guesses a letter, the corresponding blank '_' is replaced by the letter.",
+      gitURL: "https://github.com/oli-drew/Word-Guess-Game-Mini-Project",
+      depURL: "https://oli-drew.github.io/Word-Guess-Game-Mini-Project/",
       thumbnailImg: image,
       mainImg: "",
     },
