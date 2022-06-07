11a88d8d7190641576a4ba8546f1d420a24d4841
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-07 10:57:51 +0100

    Updated unit tests. Passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
-test("renders Oliver Drew | Web Developer", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Oliver Drew/i);
+test("renders About in Nav", () => {
+  render(<Nav />);
+  const linkElement = screen.getByText(/About/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Work in Nav", () => {
+  render(<Nav />);
+  const linkElement = screen.getByText(/Work/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Contact in Nav", () => {
+  render(<Nav />);
+  const linkElement = screen.getByText(/Contact/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Resume in Nav", () => {
+  render(<Nav />);
+  const linkElement = screen.getByText(/Resume/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders LinkedIn in Footer", () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/LinkedIn/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders GitHub in Footer", () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/GitHub/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Email in Footer", () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/Email/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Stack Overflow in Footer", () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/Stack Overflow/i);
   expect(linkElement).toBeInTheDocument();
 });
