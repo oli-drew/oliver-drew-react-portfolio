6b762cba9b91c2bf291118853970f0d7760dbcb5
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-09 14:50:42 +0100

    Form validation added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,68 @@
 import React, { Component } from "react";
+import validator from "validator";
 
+const resetValidation = (e) => {
+  e.target.classList.remove("is-invalid");
+  e.target.classList.remove("is-valid");
+};
+
+const validation = (e) => {
+  if (e.target.type === "email") {
+    if (validator.isEmail(e.target.value)) {
+      e.target.classList.add("is-valid");
+    } else {
+      e.target.classList.add("is-invalid");
+    }
+  } else if (e.target.value === "") {
+    e.target.classList.add("is-invalid");
+  } else {
+    e.target.classList.add("is-valid");
+  }
+};
 class ContactForm extends Component {
   render() {
     return (
       <form className="contactForm">
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" name="name" />
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            onFocus={resetValidation}
+            onBlur={validation}
+          />
+          <div className="valid-feedback">Nice name!</div>
+          <div className="invalid-feedback">Please enter a name</div>
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" className="form-control" name="email" />
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            onFocus={resetValidation}
+            onBlur={validation}
+          />
+          <div className="valid-feedback">Awesome email address!</div>
+          <div className="invalid-feedback">Please enter a valid email</div>
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          {/* <input type="text" className="form-control" name="message" /> */}
-          <textarea class="form-control" name="message"></textarea>
+          <textarea
+            className="form-control"
+            name="message"
+            onFocus={resetValidation}
+            onBlur={validation}
+          ></textarea>
+          <div className="valid-feedback">You've entered a message</div>
+          <div className="invalid-feedback">Please enter a message</div>
         </div>
-        <button type="submit" className="btn btn-outline-secondary mt-2">
+        <button
+          type="submit"
+          className="btn btn-outline-secondary mt-2"
+          disabled
+        >
           Submit
         </button>
       </form>
